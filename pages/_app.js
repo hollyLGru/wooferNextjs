41845554ec1327d3c04f95b5913b65903979f288
HollyLgru <H.grudovich@zollege.com>
2023-05-10 14:19:43 -0500

    is it working

--- pages/_app.js
+++ pages/_app.js
@@ -3,20 +3,32 @@ import React from 'react';
 import {useEffect, createContext, useState} from "react"
 
 const AuthContext = createContext();
+const petFinderKey = 'QufLibwW1Ry9MKuZ98gbJKmWW8W97OZ5QtdhUVCg9eNx7dS7J5';
+const petFinderSecret = 'QssvzFWwIoOXBNM89H19a3FyjoFfR0MsWp7k3Pcm';
+
 
 export default function App({ Component, pageProps }) {
   const [accessToken, setAccessToken] = useState(null)
 
   useEffect(() => {
     const fetchAccessToken = async () =>{
-      const res = await fetch("/api/oauth-token");
-      const json = await res.json();
-      setAccessToken(json.access_token);
+      const params = new URLSearchParams();
+      params.append("grant_type", "client_credentials");
+      params.append("client_id", petFinderKey);
+      params.append("client_secret", petFinderSecret);
+      const petFinderRes = await fetch(
+        "https://api.petfinder.com/v2/oauth2/token",
+        {
+          method: "POST",
+          body: params
+        }
+      );
+      console.log(await petFinderRes.json)
     }
     fetchAccessToken()
   }, [])
 
-  console.log(accessToken)
+
 
 
 
